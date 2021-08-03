036ac82409968dbef9f44c6f3270f1bb4c35ebd2
sql <1814294154@qq.com>
2021-08-03 20:07:49 +0800

    First Commit

---
+++ learnDemo/learnDemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learnDemo
+//
+//  Created by SQ我喜欢你 on 2021/8/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
